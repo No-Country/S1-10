77e139862a22a5a11d1f0427bd1e99a37d597eff
maegop <megordon@alu.ucam.edu>
2022-03-28 16:25:35 +0200

    page creation

---
+++ pages/problema.js
@@ -0,0 +1,70 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+  } from "@chakra-ui/react";
+  
+  export default function SimpleCard() {
+    return (
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bg={useColorModeValue("gray.50", "gray.800")}
+      >
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"3xl"}>Bienvenido</Heading>
+          </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}
+            align="center"
+          >
+            <Stack spacing={4} align="center">
+              <FormControl id="bienvenida">
+                <FormLabel fontSize={"2xl"} justify={"center"}>
+                  ¿Cuál es tu problema?
+                </FormLabel>
+              </FormControl>
+  
+              <Stack spacing={6} direction="row" align="center">
+                <Button
+                  px={38}
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                >
+                  SI
+                </Button>
+                <Button
+                  px={35}
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                >
+                  NO
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    );
+  }
+  
--- pages/registro.js
+++ pages/registro.js
@@ -92,7 +92,10 @@ export default function SignupCard() {
             </Stack>
             <Stack pt={6}>
               <Text align={"center"}>
-                Already a user? <Link color={"blue.400"}>Login</Link>
+                Already a user?{" "}
+                <Link href="/ingreso" color={"blue.400"}>
+                  Login
+                </Link>
               </Text>
             </Stack>
           </Stack>

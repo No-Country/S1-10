c406161af7fdc2b3ca0edb7d789eeb6924c0e02d
Bernal97 <84948951+Bernal97@users.noreply.github.com>
2022-03-28 18:13:25 -0300

    autenticacion y modelo

---
+++ pages/api/model/user.js
@@ -0,0 +1,9 @@
+{
+    user: {
+      firstName: string
+      lastName: string
+      email: string
+      password: string
+    },
+    expires: Date // This is the expiry of the session, not any of the tokens within the session
+  }
